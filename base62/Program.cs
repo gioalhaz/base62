b8f510f700a328c18bed942d3e55f9b3f77a1788
gioalhaz <gioalhaz@gmail.com>
2019-06-10 00:53:26 +0400

    Add project files.

---
+++ base62/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace base62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid guid = Guid.NewGuid();
+            Console.WriteLine( guid.ToString());
+
+            //ulong ul = sha;
+
+            BigInteger bint = new BigInteger(guid.ToByteArray().Concat(new byte[] { 0 }).ToArray());
+            //BigInteger bint = new BigInteger(BitConverter.GetBytes(ul).Concat(new byte[] { 0 }).ToArray());
+
+            Console.WriteLine(bint.ToString());
+
+            Console.WriteLine(ToBase62(bint));
+            //int size = sizeof(ulong);
+        }
+
+        static string ToBase62(BigInteger number)
+        {
+            var alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var n = number;
+            BigInteger basis = 62;
+            var ret = "";
+            while (n > 0)
+            {
+                BigInteger temp;
+                BigInteger.DivRem(n, basis, out temp);
+                //ulong temp = n % basis;
+                ret = alphabet[(int)temp] + ret;
+                n = BigInteger.Divide(n, basis);
+                //n = (n / basis);
+
+            }
+            return ret;
+        }
+
+        static string ToBase62(ulong number)
+        {
+            var alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var n = number;
+            ulong basis = 62;
+            var ret = "";
+            while (n > 0)
+            {
+                ulong temp = n % basis;
+                ret = alphabet[(int)temp] + ret;
+                n = (n / basis);
+
+            }
+            return ret;
+        }
+    }
+}
